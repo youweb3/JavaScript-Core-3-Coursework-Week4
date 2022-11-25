ed5c4b4c7e011b06e0d3d50e304bad6d01608292
youweb3 <yousef.shaikhan@gmail.com>
2022-11-25 23:18:34 +0000

    B- tests

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,13 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  const words = ["irina", "etza", "daniel"];
   // Act
+  const outPut = removeVowelsFromWords(words);
   // Assert
+  const expected = ["rn", "tz", "dnl"];
+
+  expect(outPut).toEqual(expected);
 });
 
 // example
--- B-write-tests/06-largest-number/largest-number.test.js
+++ B-write-tests/06-largest-number/largest-number.test.js
@@ -2,8 +2,12 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  let input = [3, 21, 88, 4, 36];
+  let expected = 88;
   // Act
+  let result = getLargestNumber(input);
   // Assert
+  expect(result).toEqual(expected);
 });
 
 // example
